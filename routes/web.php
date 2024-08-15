d6a34efd7a563c0f9895e50247c9c3451c03d154
Olya Virchenko <olyavirchenko@192.168.1.6>
2024-08-15 18:11:02 +0300

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/user/profile', [App\Http\Controllers\UserProfileController::class, 'show'])->name('user.profile');
